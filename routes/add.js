e702e927e7c49893bbe9939f2d6c46f8beed5e42
Brianna Fredeluces <bfredelu@ucsd.edu>
2021-02-04 10:56:11 -0800

    Submission commit

--- routes/add.js
+++ routes/add.js
@@ -5,14 +5,15 @@ exports.addFriend = function(request, response) { 
 	console.log(request.query.name);
 	console.log(request.query.description);
 
-	var newFriendTemp = new Object();
-	newFriendTemp.name = request.query.name;
-	newFriendTemp.description = request.query.description;
-	newFriendTemp.imageURL = 'https://loremflickr.com/500/500/girl?lock=700';
+	var newFriendName = request.query.name;
+	var newFriendDescription = request.query.description;
+	var newFriendURL = 'https://loremflickr.com/500/500/girl?lock=700';
 
-	var newFriend = JSON.stringify(newFriendTemp);
-
-	data.friends.push(JSON.parse(newFriend));
+	data.friends.push({
+		"name": newFriendName,
+		"description": newFriendDescription,
+		"imageURL": newFriendURL
+	});
 
 	response.render('index',data);
  }
